12904140931b2211c42baf56c3acfbc755c1efad
adminap <shestopalov92@gmail.com>
2026-01-14 12:32:22 +0300

    fork: PR_14.01.25. Счетчик ЧЗ в шаблон поставок + фикс html

--- Messenger/ProductSupply/NewStatusProductSupply/NewStatusProductSupplyDispatcher.php
+++ Messenger/ProductSupply/NewStatusProductSupply/NewStatusProductSupplyDispatcher.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -39,7 +39,7 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 /**
  * При присвоении поставке статуса New «Новая» -
- * бронирует ОДИН Честный знак на ОДНУ единицу продукции в поставке
+ * запускает процесс резервирования ОДИНОГО Честный знака на ОДНУ единицу продукции в поставке
  * @see ProcessReservationProductSignDispatcher
  */
 #[AsMessageHandler(priority: 0)]
--- UseCase/Admin/File/ProductSupplyFilesHandler.php
+++ UseCase/Admin/File/ProductSupplyFilesHandler.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -55,7 +55,7 @@ final readonly class ProductSupplyFilesHandler
 
         $uploadDir = implode(DIRECTORY_SEPARATOR, [
             $this->project_dir,
-            'public', 'upload', 'barcode', 'products-supply', $command->getUsr(), $command->getProfile(), 'pdf'
+            'public', 'upload', 'barcode', 'products-supply', $command->getUsr(), $command->getProfile()
         ]);
 
         /**
